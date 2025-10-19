eadfaf7860bd0d0727d0cb7a320bc05aa223562c
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-10-19 19:42:04 +0900

    feat: 도메인 값 객체 추가 - Delimiter: 구분자 패턴 보관 - Numbers: 숫자 배열 보관

---
+++ src/main/java/calculator/entity/Delimiter.java
@@ -0,0 +1,14 @@
+package calculator.entity;
+
+public class Delimiter {
+
+    private final String pattern;
+
+    public Delimiter(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+}
---
+++ src/main/java/calculator/entity/Numbers.java
@@ -0,0 +1,14 @@
+package calculator.entity;
+
+public class Numbers {
+
+    private final int[] values;
+
+    public Numbers(int[] values) {
+        this.values = values;
+    }
+
+    public int[] getValues() {
+        return values;
+    }
+}
